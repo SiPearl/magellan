a7c357f0b5d55b4b9095335e7fcae277a3ed2487
jose-angel gomez-lopez <jose-angel.gomez@sipearl.com>
2025-11-19 09:15:33 +0100

    added again the update feature patch for openbmc
    Use example:
    
    ./magellan update https://192.168.87.176:443 --username root --password 0penBmc --firmware-uri http://172.31.19.129:1337/obmc-phosphor-image-sipearl-evb0.static.mtd.tar  --scheme TFTP --insecure
    
    
    Signed-off-by: jose-angel gomez-lopez <jose-angel.gomez@sipearl.com>

--- pkg/update.go
+++ pkg/update.go
@@ -3,6 +3,10 @@ package magellan
 import (
 	"fmt"
 	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/OpenCHAMI/magellan/pkg/bmc"
 	"github.com/stmcginnis/gofish"
@@ -17,18 +21,85 @@ type UpdateParams struct {
 	Insecure         bool     // set from the --insecure flag
 }
 
+func UpdateFirmwareRemoteInsecure(q *UpdateParams) error {
+	curlcmd := "curl"
+
+	fmt.Println("INSECURE MODE (OpenBMC mode https://github.com/openbmc)")
+	// first to copy in local the file that we want to uplaod to the BMC
+	currentDir, err := os.Getwd()
+
+	fmt.Printf("currentDir %s\n", currentDir)
+	if err != nil {
+		fmt.Println(err)
+	}
+	destPath := filepath.Join(currentDir, "newfw.mtd.tar")
+
+	// fmt.Printf("destPath %s\n", destPath)
+
+	// $ curl http://172.31.19.129:1337/obmc-phosphor-image-sipearl-evb0.static.mtd.tar --output foo
+	//   % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+	//                                  Dload  Upload   Total   Spent    Left  Speed
+	// 100 36.0M  100 36.0M    0     0   901M      0 --:--:-- --:--:-- --:--:--  924M
+
+	cmd := exec.Command(curlcmd, "--output", destPath, q.FirmwareURI)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("cmd.Run() failed with %s\n", err)
+	} else {
+		fmt.Println("Firmware update initiated successfully.")
+	}
+
+	// On OpenBMC we need to execute this kind of command
+	// 	curl  -k -H "X-Auth-Token: $token"  -H "Content-Type:multipart/form-data" -X POST \
+	//  -F UpdateParameters='{"Targets":["/redfish/v1/Managers/bmc"],"@Redfish.OperationApplyTime":"Immediate"};type=application/json' \
+	//  -F 'UpdateFile=@obmc-phosphor-image-sipearl-evb0-20250304115722.static.mtd.tar;type=application/octet-stream' \
+	// https://root:0penBmc@192.168.75.39/redfish/v1/UpdateService/update
+
+	// Get BMC credentials from secret store in update parameters
+	bmcCreds, err := bmc.GetBMCCredentials(q.SecretStore, q.URI)
+	if err != nil {
+		return fmt.Errorf("failed to get BMC credentials: %w", err)
+	}
+	uri, err := url.ParseRequestURI(q.URI)
+	if err != nil {
+		return fmt.Errorf("failed to parse URI: %w", err)
+	}
+
+	prefix := "//" + bmcCreds.Username + ":" + bmcCreds.Password+ "@"
+	theurl := strings.Replace(uri.String(), "//", prefix, 1)
+	theurl += "/redfish/v1/UpdateService/update"
+	cmd = exec.Command(curlcmd, "-k", "-H \"X-Auth-Token: $token\"",
+		"-H \"Content-Type:multipart/form-data\"", "-X", "POST",
+		"-F", "UpdateFile=@newfw.mtd.tar;type=application/octet-stream",
+		"-F", "UpdateParameters={\"Targets\":[\"/redfish/v1/Managers/bmc\"],\"@Redfish.OperationApplyTime\":\"Immediate\"}",
+		theurl)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("cmd.Run() failed with %s\n", err)
+	} else {
+		fmt.Println("Firmware update initiated successfully.")
+	}
+	// remove the copy of the BMC image
+	err = os.Remove("newfw.mtd.tar")
+	if err != nil {
+		fmt.Printf("os.Remove failed with %s\n", err)
+	}
+	return nil;
+}
+
 // UpdateFirmwareRemote() uses 'gofish' to update the firmware of a BMC node.
 // The function expects the firmware URL, firmware version, and component flags to be
 // set from the CLI to perform a firmware update.
 // Example:
-// ./magellan update https://192.168.23.40 --username root --password 0penBmc
-// --firmware-url http://192.168.23.19:1337/obmc-phosphor-image.static.mtd.tar
-// --scheme TFTP
-//
-// being:
-// q.URI https://192.168.23.40
-// q.TransferProtocol TFTP
-// q.FirmwarePath http://192.168.23.19:1337/obmc-phosphor-image.static.mtd.tar
+// ./magellan/magellan update https://192.168.87.176:443 --username root
+// --password 0penBmc
+// --firmware-uri http://172.31.19.129:1337/obmc-phosphor-image-sipearl-evb0.static.mtd.tar
+// --scheme TFTP --insecure
 func UpdateFirmwareRemote(q *UpdateParams) error {
 	// parse URI to set up full address
 	uri, err := url.ParseRequestURI(q.URI)
@@ -42,6 +113,15 @@ func UpdateFirmwareRemote(q *UpdateParams) error {
 		return fmt.Errorf("failed to get BMC credentials: %w", err)
 	}
 
+	if (q.Insecure) {
+		fmt.Printf("INSECURE\n")
+		UpdateFirmwareRemoteInsecure(q);
+		return nil;
+	} else {
+		fmt.Printf("SECURE MODE\n")
+	}
+
+	// SECURE MODE
 	// Connect to the Redfish service using gofish
 	client, err := gofish.Connect(gofish.ClientConfig{Endpoint: uri.String(), Username: bmcCreds.Username, Password: bmcCreds.Password, Insecure: q.Insecure})
 	if err != nil {
@@ -57,6 +137,7 @@ func UpdateFirmwareRemote(q *UpdateParams) error {
 
 	// Build the update request payload
 	req := redfish.SimpleUpdateParameters{
+		ForceUpdate:      true,
 		ImageURI:         q.FirmwareURI,
 		TransferProtocol: redfish.TransferProtocolType(q.TransferProtocol),
 	}
